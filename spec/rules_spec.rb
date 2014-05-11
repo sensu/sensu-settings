443c63679ff47531fda52e4ba2d2fc83db9808dd
Sean Porter <portertech@gmail.com>
2014-05-10 23:25:26 -0700

    test all of the rules

--- spec/rules_spec.rb
+++ spec/rules_spec.rb
@@ -10,13 +10,23 @@ describe "Sensu::Settings::Rules" do
     must_be_a_hash("").should be_false
     must_be_an_array([]).should be_true
     must_be_an_array("").should be_false
+    must_be_an_array_if_set([]).should be_true
+    must_be_an_array_if_set(nil).should be_true
+    must_be_an_array_if_set("").should be_false
     must_be_a_string("").should be_true
     must_be_a_string(1).should be_false
+    must_be_a_string_if_set("").should be_true
+    must_be_a_string_if_set(nil).should be_true
+    must_be_a_string_if_set(1).should be_false
     must_be_an_integer(1).should be_true
     must_be_an_integer("").should be_false
     must_be_a_numeric(1.5).should be_true
     must_be_a_numeric("").should be_false
     must_match_regex(/^foo$/, "foo").should be_true
     must_match_regex(/^foo$/, "bar").should be_false
+    must_be_boolean_if_set(true).should be_true
+    must_be_boolean_if_set(false).should be_true
+    must_be_boolean_if_set(nil).should be_true
+    must_be_boolean_if_set("").should be_false
   end
 end
