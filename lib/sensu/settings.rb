397be92317117e0e5acdce6308b1446abd159ba5
Sean Porter <portertech@gmail.com>
2014-05-26 21:33:22 -0700

    Settings.get() semi-singleton-pattern

--- lib/sensu/settings.rb
+++ lib/sensu/settings.rb
@@ -2,29 +2,41 @@ require "sensu/settings/loader"
 
 module Sensu
   module Settings
-    # Load Sensu settings.
-    #
-    # @param [Hash] options
-    # @option options [String] :config_file to load.
-    # @option options [String] :config_dir to load.
-    # @option options [Array] :config_dirs to load.
-    # @return [Loader] a loaded instance of Loader.
-    def self.load(options={})
-      loader = Loader.new
-      loader.load_env
-      if options[:config_file]
-        loader.load_file(options[:config_file])
-      end
-      if options[:config_dir]
-        loader.load_directory(options[:config_dir])
-      end
-      if options[:config_dirs]
-        options[:config_dirs].each do |directory|
-          loader.load_directory(directory)
+    class << self
+      # Load Sensu settings.
+      #
+      # @param [Hash] options
+      # @option options [String] :config_file to load.
+      # @option options [String] :config_dir to load.
+      # @option options [Array] :config_dirs to load.
+      # @return [Loader] a loaded instance of Loader.
+      def load(options={})
+        @loader = Loader.new
+        @loader.load_env
+        if options[:config_file]
+          @loader.load_file(options[:config_file])
         end
+        if options[:config_dir]
+          @loader.load_directory(options[:config_dir])
+        end
+        if options[:config_dirs]
+          options[:config_dirs].each do |directory|
+            @loader.load_directory(directory)
+          end
+        end
+        @loader.set_env!
+        @loader
       end
-      loader.set_env!
-      loader
+
+      # Retrieve the current loaded settings loader or load one up if
+      # there isn't one. Note: We may need to add a mutex for thread
+      # safety.
+      #
+      # @param [Hash] options to pass to load().
+      # @return [Loader] instance of a loaded loader.
+      def get(options={})
+        @loader || load(options)
+      end
     end
   end
 end
--- spec/settings_spec.rb
+++ spec/settings_spec.rb
@@ -18,6 +18,12 @@ describe "Sensu::Settings" do
     settings.should respond_to(:validate)
   end
 
+  it "can retrive the current loaded loader" do
+    settings = Sensu::Settings.load
+    Sensu::Settings.get.should eq(settings)
+    Sensu::Settings.get.should eq(settings)
+  end
+
   it "can load settings from the environment, a file, and a directory" do
     ENV["RABBITMQ_URL"] = "amqp://guest:guest@localhost:5672/"
     settings = Sensu::Settings.load(:config_file => @config_file, :config_dir => @config_dir)
