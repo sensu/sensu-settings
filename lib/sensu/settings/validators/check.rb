c46fc83f2f680323955f152de10e0474a420b855
rmc3 <rich.m.chatterton@gmail.com>
2015-03-14 18:13:55 -0500

    Add separate validation checks for check[:command] and check[:extension].

--- lib/sensu/settings/validators/check.rb
+++ lib/sensu/settings/validators/check.rb
@@ -61,8 +61,12 @@ module Sensu
             invalid(check, "check name must be a string")
           must_match_regex(/^[\w\.-]+$/, check[:name]) ||
             invalid(check, "check name cannot contain spaces or special characters")
+          must_be_a_string_if_set(check[:command]) ||
+            invalid(check, "check command must be a string")
+          must_be_a_string_if_set(check[:extension]) ||
+            invalid(check, "check extension must be a string")
           (must_be_a_string(check[:command]) ^ must_be_a_string(check[:extension])) ||
-            invalid(check, "one of check command or check extension must be a string")
+            invalid(check, "one of check command or check extension must be set and a string, but not both")
           must_be_a_numeric_if_set(check[:timeout]) ||
             invalid(check, "check timeout must be numeric")
           must_be_a_string_if_set(check[:source]) ||
